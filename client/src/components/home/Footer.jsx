7f7b2f6dadd4f0a146f2279ef640e1b47ef27005
solana315 <pv27649@alunos.estgv.ipv.pt>
2026-09-05 18:05:55 +0100

    2026

--- client/src/components/home/Footer.jsx
+++ client/src/components/home/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="site-footer container">
       <Logo />
-      <span>feito para viajar devagar · 2024</span>
+      <span>Viagem 2026</span>
       <a href="#top">
         voltar ao topo <ArrowUpRight size={14} />
       </a>
